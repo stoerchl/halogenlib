9bd38da530fccda2599354e2f93000287b02a1cd
stoerchl <9351883+stoerchl@users.noreply.github.com>
2022-05-16 11:58:47 +0200

    Analysing multiple files using the same class instance

--- lib/generator.py
+++ lib/generator.py
@@ -9,14 +9,15 @@ def yara_image_rule_maker(self) -> dict:
     """ Yara image rule generator
     returns: rule_match_dict """
     if self.image_name is None:
-        find_matches_dict = self.get_file[1]
         self.image_name = []
-        for values in find_matches_dict:
-            if len(values[0]) > 0:
-                if values == "JPG2" and ("JPG" in self.image_name):
-                    pass  #skip jpg2 because we matched on a more narrow jpg header value.
-                else:
-                    self.image_name.append(values)
+
+    find_matches_dict = self.get_file[1]
+    for values in find_matches_dict:
+        if len(values[0]) > 0:
+            if values == "JPG2" and ("JPG" in self.image_name):
+                pass  #skip jpg2 because we matched on a more narrow jpg header value.
+            else:
+                self.image_name.append(values)
 
     rule_match_dict = yara_image_generator(self)
     return rule_match_dict
@@ -28,16 +29,19 @@ def yara_image_generator(self) -> dict:
     dict_list = []
     if self.image_name is not None:
         for ftype in self.image_name:
-            for match in self.get_file[1][ftype]:
-                rule_data = {}
-                rule_data['format'] = ftype
-                if type(match) is bytes:
-                    img_hex_value = binascii.hexlify(match)
-                    value = (str(img_hex_value))
-                    rule_data['hex'] = value[2:-1]
-                elif type(match) is str:
-                    rule_data['hex'] = match
-                if rule_data not in dict_list:
-                    dict_list.append(rule_data)
+            try:
+                for match in self.get_file[1][ftype]:
+                    rule_data = {}
+                    rule_data['format'] = ftype
+                    if type(match) is bytes:
+                        img_hex_value = binascii.hexlify(match)
+                        value = (str(img_hex_value))
+                        rule_data['hex'] = value[2:-1]
+                    elif type(match) is str:
+                        rule_data['hex'] = match
+                    if rule_data not in dict_list:
+                        dict_list.append(rule_data)
+            except:
+                pass
     return dict_list
 
